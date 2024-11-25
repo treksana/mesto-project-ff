000aeec85881adb4f78270d7b43bc773df8415f1
treksana <zemlyanika498@gmail.com>
2024-11-25 22:54:33 +0500

    Creation and removal of cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsContainer = document.querySelector('.places__list');
+
+function displayCards(initialCards) {
+const cardElements = initialCards.map(createCard); 
+cardsContainer.append(...cardElements);
+}
+
+function createCard({name, link}) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  return cardElement;
+}
+
+cardsContainer.addEventListener('click', function(event) {
+  if (event.target.classList.contains('card__delete-button')) {
+    deleteCard(event);
+  }
+});
+
+function deleteCard(event) {
+  const cardElement = event.target.closest('.places__item');
+  cardElement.remove();
+}
+  
+displayCards(initialCards);
